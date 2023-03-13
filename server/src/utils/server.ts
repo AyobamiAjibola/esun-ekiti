d18bb08afe4ea07c9a366ec99042bbe6abd5d286
ayobami ajibola <ayobamiajibola46@gmail.com>
2023-03-13 03:34:38 +0100

    proxy to package.json

--- server/src/utils/server.ts
+++ server/src/utils/server.ts
@@ -17,10 +17,10 @@ export const corsOptions = {
   origin: [
     'http://localhost:3001',
     'http://localhost:3000',
-    'https://esun-ekiti-portal.onrender.com',
-    <string>process.env.ADMIN_PORTAL
+    // 'https://esun-ekiti-portal.onrender.com',
+    // <string>process.env.ADMIN_PORTAL
   ],
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  // methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true,
 };
 
