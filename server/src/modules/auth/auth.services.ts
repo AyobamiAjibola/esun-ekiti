b775ef31c65c5e53279a86cc587cbe7a5fdb29b3
ayobami ajibola <ayobamiajibola46@gmail.com>
2023-03-29 20:51:43 +0100

    cookie wahala

--- server/src/modules/auth/auth.services.ts
+++ server/src/modules/auth/auth.services.ts
@@ -30,7 +30,8 @@ export const adminLogin = async (res: Response, body: AuthType, next: NextFuncti
       maxAge: 7 * 24 * 60 * 60 * 1000, //7 days
       secure: true,
       sameSite: 'none',
-      path:'/'
+      path:'/',
+      domain: 'esun-ekiti-portal.onrender.com'
     });
 
     return {token};
