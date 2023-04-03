12591a732998a92a4a143c5aafca5424913f5a44
ayobami ajibola <ayobamiajibola46@gmail.com>
2023-04-03 23:57:46 +0100

    .

--- server/src/utils/connect.ts
+++ server/src/utils/connect.ts
@@ -4,7 +4,7 @@ import log from "./logger";
 const connect = async () => {
   try {
     await database.init();
-    // await database.sequelize.sync({alter: true});
+    await database.sequelize.sync({alter: true});
     log.info("Connected to database successfully");
   } catch (error) {
     log.error("Could not connect to DB");
