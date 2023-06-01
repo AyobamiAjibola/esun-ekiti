8ed2b48dafe2b8817334088e153c03032c19207f
Ayobami Ajibola <ayobamiajibola@gmail.com>
2023-06-01 00:03:55 +0000

    .

--- server/src/utils/server.ts
+++ server/src/utils/server.ts
@@ -15,8 +15,8 @@ dotenv.config({ path: resolve(__dirname, "../.env") });
 
 export const corsOptions = {
   origin: [
-    <string>process.env.CLIENT_URL,
-    <string>process.env.ADMIN_URL
+    'http://18.117.9.206:8083',
+    'http://18.117.9.206:8084'
   ],
   credentials: true,
 };
