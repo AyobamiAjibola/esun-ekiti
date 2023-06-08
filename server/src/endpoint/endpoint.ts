b4703cf1df843d492c3aaf0d9ae978410efd789a
AyobamiAjibola <ayobamiajibola46@gmail.com>
2023-06-08 20:11:44 +0100

    .

--- server/src/endpoint/endpoint.ts
+++ server/src/endpoint/endpoint.ts
@@ -9,21 +9,13 @@ import historyRoutes from "../routes/history/history.route";
 import settings from "../config/settings";
 
 const routes = (app: Express) => {
-  // app.use(`${settings.service.apiRoot}/auth`, authRoutes);
-  // app.use(`${settings.service.apiRoot}/user`, userRoutes);
-  // app.use(`${settings.service.apiRoot}/council`, councilRoutes);
-  // app.use(`${settings.service.apiRoot}/event`, eventRoutes);
-  // app.use(`${settings.service.apiRoot}/news`, newsRoutes);
-  // app.use(`${settings.service.apiRoot}/project`, projectRoutes);
-  // app.use(`${settings.service.apiRoot}/history`, historyRoutes);
-
-  app.use(`/auth`, authRoutes);
-  app.use(`/user`, userRoutes);
-  app.use(`/council`, councilRoutes);
-  app.use(`/event`, eventRoutes);
-  app.use(`/news`, newsRoutes);
-  app.use(`/project`, projectRoutes);
-  app.use(`/history`, historyRoutes);
+  app.use(`${settings.service.apiRoot}/auth`, authRoutes);
+  app.use(`${settings.service.apiRoot}/user`, userRoutes);
+  app.use(`${settings.service.apiRoot}/council`, councilRoutes);
+  app.use(`${settings.service.apiRoot}/event`, eventRoutes);
+  app.use(`${settings.service.apiRoot}/news`, newsRoutes);
+  app.use(`${settings.service.apiRoot}/project`, projectRoutes);
+  app.use(`${settings.service.apiRoot}/history`, historyRoutes);
 };
 
 export default routes;
