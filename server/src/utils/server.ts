76cb6261ea1830213c0eaaad6ac16f1013ef55cb
Ayobami Ajibola <ayobamiajibola@gmail.com>
2023-06-08 17:54:53 +0000

    .

--- server/src/utils/server.ts
+++ server/src/utils/server.ts
@@ -33,6 +33,13 @@ const createServer = () => {
   app.use(cookieParser() as express.RequestHandler);
   app.use(helmet({ crossOriginEmbedderPolicy: false }));
   app.use("/uploads", express.static("uploads"));
+  
+ // app.use(function(req, res, next) {
+   // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+   // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+   // res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+   // next();
+ // });
 
   routes(app);
 
